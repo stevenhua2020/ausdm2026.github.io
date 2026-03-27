e0e0e5dc2fc46237011e3914f4d16cfcf225d095
stevenhua2020 <69631640+stevenhua2020@users.noreply.github.com>
2026-03-27 11:40:16 +1100

    Update nav.js
    added publication section after submission

--- docs/component/nav.js
+++ docs/component/nav.js
@@ -20,6 +20,7 @@ templateNavDoc.documentElement.innerHTML = `
                         <li><a href="./Call for Papers.html">Call for Papers</a></li>
                         <li><a href="./Call for Doctoral Consortium.html">Call for Doctoral Consortium</a></li>
                         <li><a href="./Submissions.html">Submissions</a></li>
+                        <li><a href="./Publications.html">Publication</a></li>
                         <!-- <li><a href="./Call for Tutorials and Workshops.html">Call for Tutorials and Workshops</a></li>
                         <li><a href="./Special Sessions.html">Special Sessions</a></li> -->
                     </ul>
